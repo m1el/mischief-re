e71bf4312208bea3094999ff64ce7e2146917b89
Igor null <m1el@ya.ru>
2015-04-21 12:52:26 +0300

    added parsing unpacked data parsing

--- artparser.py
+++ artparser.py
@@ -509,6 +509,187 @@ def mischief_unpack(byte_input):
 
 ART_MAGICS = set([b'\xc5\xb3\x8b\xe9', b'\xc5\xb3\x8b\xe7'])
 
+def read_byte(data, pos):
+    return (data[pos], pos+1)
+
+
+def read_int(data, pos):
+    (val,) = struct.unpack('I', data[pos:pos+4])
+    return (val, pos+4)
+
+
+def read_int_array(data, pos, count):
+    val = list(struct.unpack('%dI'%count,
+                             data[pos:pos+4*count]))
+    return (val, pos+4*count)
+
+
+def read_float(data, pos):
+    (val,) = struct.unpack('f', data[pos:pos+4])
+    return (val, pos+4)
+
+
+def read_float_array(data, pos, n):
+    floats = list(struct.unpack('%df'%n,
+                                data[pos:pos+4*n]))
+    return (floats, pos+4*n)
+
+
+def read_float_matrix(data, pos, n, m):
+    floats = list(struct.unpack('%df'%(n*m),
+                                data[pos:pos+4*n*m]))
+    return ([floats[i*n:i*n+n] for i in range(0,m)],
+            pos+4*n*m)
+
+
+def read_bytes(data, pos, length):
+    return (data[pos:pos+length], pos+length)
+
+
+def read_string(data, pos, length):
+    val = data[pos:pos+length]
+    return (val.split(b'\x00', 1)[0].decode('utf-8'),
+            pos+length)
+
+
+def read_color(data, pos):
+    val = (data[pos], data[pos+1], data[pos+2])
+    return (val, pos+3)
+
+
+def read_pen_info(data, pos):
+    val = {}
+    (val['type'], pos) = read_int(data, pos)
+    (val['color'], pos) = read_color(data, pos)
+    (val['noise'], pos) = read_float(data, pos)
+    (val['size'], pos) = read_float(data, pos)
+    (val['size_min'], pos) = read_float(data, pos)
+    (val['opacity'], pos) = read_float(data, pos)
+    (val['opacity_min'], pos) = read_float(data, pos)
+    (val['is_eraser'], pos) = read_int(data, pos)
+    return (val, pos)
+
+
+def read_layer_info(data, pos):
+    val = {}
+    (val['visible'], pos) = read_int(data, pos)
+    (val['opacity'], pos) = read_float(data, pos)
+    (val['name'], pos) = read_string(data, pos, 256)
+    (val['action_count'], pos) = read_int(data, pos)
+    (val['matrix'], pos) = read_float_matrix(data, pos, 4, 4)
+    (val['zoom'], pos) = read_float(data, pos)
+    return (val, pos)
+
+
+def read_image(data, pos):
+    val = {}
+    (val['type'], pos) = read_int(data, pos)
+    (size, pos) = read_int(data, pos)
+    (val['raw'], pos) = read_bytes(data, pos, size)
+    return (val, pos)
+
+
+def read_action(data, pos):
+    val = {}
+    (val['layer'], pos) = read_int(data, pos)
+    (val['action_id'], pos) = read_int(data, pos)
+
+    if val['action_id'] == 0x01:
+        val['action_name'] = 'stroke'
+        (point_count, pos) = read_int(data, pos)
+        points = []
+        point = {}
+        (point['x'], pos) = read_float(data, pos)
+        (point['y'], pos) = read_float(data, pos)
+        (point['p'], pos) = read_float(data, pos)
+        points.append(point)
+
+        x = point['x']
+        y = point['y']
+
+        for i in range(point_count - 1):
+            (tmp, pos) = read_int(data, pos)
+            (byt, pos) = read_byte(data, pos)
+            dx = tmp & 0x3fff
+            if tmp & (1<<14): dx = -dx
+            dy = (tmp >> 15) & 0x3fff
+            if tmp & (tmp << 29): dy = -dy
+            p = (tmp >> 30) | (byt << 2)
+            x += dx/32.
+            y += dy/32.
+            points.append({'x': x, 'y': y, 'p': p/0x3ff})
+
+        val['points'] = points
+
+    elif val['action_id'] == 0x08:
+        val['action_name'] = 'unknown_08'
+        (val['argument'], pos) = read_int(data, pos)
+
+    elif val['action_id'] == 0x33:
+        val['action_name'] = 'view_matrix'
+        (val['matrix'], pos) = read_float_matrix(data, pos, 4, 4)
+        (val['zoom'], pos) = read_float(data, pos)
+
+    elif val['action_id'] == 0x34:
+        val['action_name'] = 'pen_properties'
+        (val['type'], pos) = read_int(data, pos)
+        (val['noise'], pos) = read_float(data, pos)
+        (val['size'], pos) = read_float(data, pos)
+        (val['size_min'], pos) = read_float(data, pos)
+        (val['opacity'], pos) = read_float(data, pos)
+        (val['opacity_min'], pos) = read_float(data, pos)
+
+    elif val['action_id'] == 0x35:
+        val['action_name'] = 'pen_color'
+        (val['color'], pos) = read_color(data, pos)
+
+    elif val['action_id'] == 0x36:
+        val['action_name'] = 'is_eraser'
+        (val['is_eraser'], pos) = read_int(data, pos)
+
+    elif val['action_id'] == 0x0f:
+        val['action_name'] = 'paste_layer'
+        (val['from_layer'], pos) = read_int(data, pos)
+        (val['rect'], pos) = read_float_array(data, pos, 4)
+        (val['matrix_1'], pos) = read_float_matrix(data, pos, 4, 4)
+        (val['zoom_1'], pos) = read_float(data, pos)
+        (val['matrix_2'], pos) = read_float_matrix(data, pos, 4, 4)
+        (val['zoom_2'], pos) = read_float(data, pos)
+
+    elif val['action_id'] == 0x0d:
+        val['action_name'] = 'layer_matrix'
+        (val['matrix'], pos) = read_float_matrix(data, pos, 4, 4)
+        (val['zoom'], pos) = read_float(data, pos)
+
+    elif val['action_id'] == 0x0e:
+        val['action_name'] = 'cut'
+        (val['rect'], pos) = read_float_array(data, pos, 4)
+
+    elif val['action_id'] == 0x0c:
+        val['action_name'] = 'merge_layer'
+        (val['from_layer'], pos) = read_int(data, pos)
+        (val['opacity_src'], pos) = read_float(data, pos)
+        (val['opactty_dst'], pos) = read_float(data, pos)
+        (val['matrix'], pos) = read_float_matrix(data, pos, 4, 4)
+        (val['zoom'], pos) = read_float(data, pos)
+
+    elif val['action_id'] == 0x07:
+        val['action_name'] = 'draw_image'
+        (val['dst_center'], pos) = read_float_array(data, pos, 2)
+        (val['dst_size'], pos) = read_float_array(data, pos, 2)
+        (val['unknown'], pos) = read_int(data, pos)
+        (val['src_size'], pos) = read_int_array(data, pos, 2)
+        (val['image_id'], pos) = read_int(data, pos)
+
+    else:
+        import binascii
+        print('unknown action: %x' % val['action_id'])
+        print(binascii.hexlify(data[pos:pos+200]))
+        die()
+
+    return (val, pos)
+
+
 class ArtParser(object):
     '''
     Class for parsing an .art file.
@@ -516,6 +697,25 @@ class ArtParser(object):
     '''
     data = None
     raw_size = 0
+    version = None
+    active_layer_num = None
+    unknown_08 = None
+    background_color = None
+    background_alpha = None
+    unknown_13 = None
+    unknown_17 = None
+    unknown_1b = None
+    unknown_1f = None
+    pen_info = None
+    unknown_42 = None
+    unknonw_46 = None
+    view_matrix = None
+    view_zoom = None
+    layer_order = None
+    layers = None
+    images = None
+    actions = None
+    unknown_eof = None
 
     def __init__(self, fname):
         with open(fname, 'rb') as fd:
@@ -528,21 +728,75 @@ class ArtParser(object):
             (self.raw_size,) = struct.unpack('I', header[0x24:0x28])
             self.data = fd.read(self.raw_size)
             self.data = mischief_unpack(self.data)
-            # self.parse_unpacked()
+            self.parse_unpacked()
 
-    def parse_unpacked(data):
-        pass
+
+    def parse_unpacked(self):
+        pos = 0
+        data = self.data
+        (self.version, pos) = read_int(data, pos)
+        (self.active_layer_num, pos) = read_int(data, pos)
+        (self.unknonw_08, pos) = read_int(data, pos)
+        (self.background_color, pos) = read_color(data, pos)
+        (self.background_alpha, pos) = read_float(data, pos)
+        (self.unknown_13, pos) = read_int(data, pos)
+        (self.unknown_17, pos) = read_int(data, pos)
+        (self.unknown_1b, pos) = read_int(data, pos)
+        (self.unknown_1f, pos) = read_int(data, pos)
+        (self.pen_info, pos) = read_pen_info(data, pos)
+        (self.unknown_42, pos) = read_int(data, pos)
+        (self.unknown_46, pos) = read_float(data, pos)
+        (self.view_matrix, pos) = read_float_matrix(data, pos, 4, 4)
+        (self.view_zoom, pos) = read_float(data, pos)
+        (order_count, pos) = read_int(data, pos)
+        (self.layer_order, pos) = read_int_array(data, pos, order_count)
+
+        (layer_count, pos) = read_int(data, pos)
+        self.layers = []
+
+        for i in range(layer_count):
+            (layer_info, pos) = read_layer_info(data, pos)
+            self.layers.append(layer_info)
+
+        (images_count, pos) = read_int(data, pos)
+        self.images = []
+
+        for i in range(images_count):
+            (image, pos) = read_image(data,pos)
+            self.images.append(image)
+
+        (action_count, pos) = read_int(data, pos)
+        self.actions = []
+
+        for i in range(action_count):
+            (action, pos) = read_action(data, pos)
+            self.actions.append(action)
+
+        (self.unknown_eof, pos) = read_int(data, pos)
 
 
 # simple wrapper for calling this file from command line
 def main(argv):
+    from pprint import pprint
     if len(argv) < 2:
         print('usage: artparser.py <input file>')
         return 1
 
     art = ArtParser(argv[1])
-    output = getattr(sys.stdout, 'buffer', sys.stdout)
-    output.write(art.data)
+    # output = getattr(sys.stdout, 'buffer', sys.stdout)
+    # output.write(art.data)
+    print('pen info:')
+    pprint(art.pen_info)
+    print('view matrix:')
+    pprint(art.view_matrix)
+    print('layer order:')
+    pprint(art.layer_order)
+    print('layer info:')
+    pprint(art.layers)
+    # print('images:')
+    # pprint(art.images)
+    print('actions:')
+    pprint(art.actions)
 
 
 if __name__ == '__main__':
