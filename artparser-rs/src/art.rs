c863205d60772362721684382173fd3de2901424
Igor null <m1el.2027@gmail.com>
2019-07-17 00:17:28 +0300

    re-implemented the parser using nom

--- artparser-rs/src/art.rs
+++ artparser-rs/src/art.rs
@@ -1,18 +1,14 @@
-use std::io::{self, Cursor, Read};
-use std::fs::{File};
-use std::path::{Path};
-use byteorder::{ReadBytesExt, LittleEndian};
-use std::string::{FromUtf8Error};
-use std::str::{Utf8Error};
-use lzunpack::{DecodeError, decompress};
+use std::{
+    io::{self, Read},
+    fs::File,
+    path::Path,
+};
 
-const ART_MAGICS: &[u32] = &[
-    0xe78bb3c5, // hex c5b38be7
-    0xe98bb3c5, // hex c6b38be9
-];
+use lzunpack::{decompress, DecodeError};
+use parser::{read_compressed, read_content};
 
 pub struct ArtFile {
-    pub version: u32,
+    pub version: usize,
     pub active_layer: usize,
     pub background_color: RGB,
     pub background_alpha: f32,
@@ -23,194 +19,29 @@ pub struct ArtFile {
     pub layer_order: Vec<usize>,
     pub layers: Vec<LayerInfo>,
     pub images: Vec<Image>,
-    pub actions: Vec<Action>,
+    pub actions: Vec<(usize, Action)>,
 }
 
 impl ArtFile {
     pub fn from_path<P: AsRef<Path>>(path: P) -> Result<ArtFile, ArtError> {
-        ArtFile::from_input(&mut File::open(path)?)
+        let mut buf = Vec::new();
+        let mut file = File::open(path)?;
+        file.read_to_end(&mut buf)?;
+        ArtFile::from_bytes(&buf[..])
     }
+    pub fn from_bytes(bytes: &[u8]) -> Result<ArtFile, ArtError> {
+        let (compressed, (_ver, pins, _rest_len)) = read_compressed(bytes)
+            .map_err(|_e| ArtError::ParsingError(String::from("failed parse compressed")))?;
 
-    pub fn from_input<R: Read>(input: &mut R) -> Result<ArtFile, ArtError> {
-        let mut version_header = [0_u8; 8];
-        input.read_exact(&mut version_header[..])?;
+        let decompressed = decompress(compressed)?;
 
-        let cursor = &mut &version_header[..];
-        let magic = read_u32(cursor)?;
-        let version = read_u32(cursor)?;
-        if !ART_MAGICS.contains(&magic) {
-            return Err(ArtError::BadMagic(magic));
-        }
+        let (_, mut art) = read_content(&decompressed[..])
+            .map_err(|_e| ArtError::ParsingError(String::from("failed parse decompressed")))?;
 
-        let pins;
-        if version & 0xFF == 0 {
-            let mut header = vec![0_u8; 0x08];
-            input.read_exact(&mut header)?;
-            pins = Vec::new();
-        } else if version == 0x81 {
-            let mut header = vec![0_u8; 0x1c];
-            input.read_exact(&mut header)?;
-            pins = Vec::new();
-        } else if version == 0x82 {
-            let mut header = vec![0_u8; 0x21];
-            input.read_exact(&mut header)?;
-            pins = read_pins(input)?;
-        } else {
-            return Err(ArtError::BadVersion(version));
-        }
+        art.pins = pins;
 
-        let raw_size = read_u32(input)?;
-        let mut compressed = vec![0_u8; raw_size as usize];
-        input.read_exact(&mut compressed)?;
-
-        let decompressed = decompress(&compressed)?;
-        let cursor = &mut Cursor::new(&decompressed[..]);
-
-        let version = read_u32(cursor)?;
-        let active_layer = read_u32(cursor)? as usize;
-        let _unknown08 = read_u32(cursor)?;
-        let background_color = read_rgb(cursor)?;
-        let background_alpha = read_f32(cursor)?;
-        let _unknown13 = read_u32(cursor)?;
-        let _unknown17 = read_u32(cursor)?;
-        let _unknown1b = read_u32(cursor)?;
-        let _unknown1f = read_u32(cursor)?;
-        let pen_info = read_pen_info(cursor)?;
-        let _unknown42 = read_u32(cursor)?;
-        let _unknown46 = read_u32(cursor)?;
-        let view_matrix = read_matrix(cursor)?;
-        let view_zoom = read_f32(cursor)?;
-
-        let order_count = read_u32(cursor)?;
-        let mut layer_order = Vec::new();
-        for _ in 0..order_count {
-            layer_order.push(read_u32(cursor)? as usize);
-        }
-
-        let layer_count = read_u32(cursor)?;
-        let mut layers = Vec::new();
-        for _ in 0..layer_count {
-            layers.push(read_layer_info(cursor)?);
-        }
-
-        let image_count = read_u32(cursor)?;
-        let mut images = Vec::new();
-        for _ in 0..image_count {
-            images.push(read_image(cursor)?);
-        }
-
-        let action_count = read_u32(cursor)?;
-        let mut actions = Vec::new();
-        for _ in 0..action_count {
-            actions.push(read_action(cursor)?);
-        }
-
-        Ok(ArtFile {
-            version,
-            active_layer,
-            background_color,
-            background_alpha,
-            pen_info,
-            view_matrix,
-            view_zoom,
-            pins,
-            layer_order,
-            layers,
-            images,
-            actions,
-        })
+        Ok(art)
     }
-}
-
-#[derive(Debug)]
-pub enum ArtError {
-    IoError(io::Error),
-    FromUtf8Error(FromUtf8Error),
-    Utf8Error(Utf8Error),
-    DecompressError(DecodeError),
-    BadMagic(u32),
-    BadVersion(u32),
-    BadAction(u32),
-}
-impl From<io::Error> for ArtError {
-    fn from(err: io::Error) -> ArtError { ArtError::IoError(err) }
-}
-impl From<FromUtf8Error> for ArtError {
-    fn from(err: FromUtf8Error) -> ArtError { ArtError::FromUtf8Error(err) }
-}
-impl From<Utf8Error> for ArtError {
-    fn from(err: Utf8Error) -> ArtError { ArtError::Utf8Error(err) }
-}
-impl From<DecodeError> for ArtError {
-    fn from(err: DecodeError) -> ArtError { ArtError::DecompressError(err) }
-}
-
-fn read_u8<R: Read>(input: &mut R) -> Result<u8, ArtError> {
-    let buf = &mut [0_u8];
-    input.read_exact(buf)?;
-    Ok(buf[0])
-}
-
-fn read_u32<R: Read>(input: &mut R) -> Result<u32, ArtError> {
-    Ok(input.read_u32::<LittleEndian>()?)
-}
-
-fn read_f32<R: Read>(input: &mut R) -> Result<f32, ArtError> {
-    Ok(input.read_f32::<LittleEndian>()?)
-}
-
-fn read_rect<R: Read>(input: &mut R) -> Result<[f32; 4], ArtError> {
-    let mut rect = [0.0; 4];
-    for idx in 0..4 {
-        rect[idx] = read_f32(input)?;
-    }
-
-    Ok(rect)
-}
-
-fn read_matrix<R: Read>(input: &mut R) -> Result<[f32; 16], ArtError> {
-    let mut matrix = [0.0; 16];
-    for idx in 0..16 {
-        matrix[idx] = read_f32(input)?;
-    }
-
-    Ok(matrix)
-}
-
-fn read_const_string<R: Read>(input: &mut R) -> Result<String, ArtError> {
-    let mut buf = [0_u8; 256];
-    input.read_exact(&mut buf)?;
-    let start = buf[..].split(|&c| c == 0).next().unwrap();
-    Ok(std::str::from_utf8(&start)?.to_string())
-}
-
-fn read_string<R: Read>(input: &mut R) -> Result<String, ArtError> {
-    let count = read_u32(input)?;
-    let mut buf = vec![0_u8; count as usize];
-    input.read_exact(&mut buf)?;
-    println!("{:?}", buf);
-    let mut rv = String::from_utf8(buf)?;
-    rv.pop();
-    Ok(rv)
-}
-
-#[derive(Debug)]
-pub struct ArtPin {
-    pub matrix: [f32; 16],
-    pub name: String,
-}
-
-fn read_pins<R: Read>(input: &mut R) -> Result<Vec<ArtPin>, ArtError> {
-    let count = read_u32(input)?;
-    let mut pins = Vec::with_capacity(count as usize);
-
-    for _ in 0..count {
-        let matrix = read_matrix(input)?;
-        let name = read_string(input)?.trim().to_string();
-        pins.push(ArtPin { matrix, name });
-    }
-
-    Ok(pins)
 }
 
 #[derive(Debug)]
@@ -218,16 +49,6 @@ pub struct RGB {
     pub r: u8,
     pub g: u8,
     pub b: u8,
-}
-
-fn read_rgb<R: Read>(input: &mut R) -> Result<RGB, ArtError> {
-    let mut buf = [0_u8; 3];
-    input.read_exact(&mut buf[..])?;
-    Ok(RGB {
-        r: buf[0],
-        g: buf[1],
-        b: buf[2],
-    })
 }
 
 #[derive(Debug)]
@@ -242,17 +63,10 @@ pub struct PenInfo {
     pub is_eraser: bool,
 }
 
-fn read_pen_info<R: Read>(input: &mut R) -> Result<PenInfo, ArtError> {
-    Ok(PenInfo {
-        kind: read_u32(input)?,
-        color: read_rgb(input)?,
-        noise: read_f32(input)?,
-        size: read_f32(input)?,
-        size_min: read_f32(input)?,
-        opacity: read_f32(input)?,
-        opacity_min: read_f32(input)?,
-        is_eraser: read_u32(input)? != 0,
-    })
+#[derive(Debug)]
+pub struct ArtPin {
+    pub matrix: [f32; 16],
+    pub name: String,
 }
 
 #[derive(Debug)]
@@ -265,48 +79,28 @@ pub struct LayerInfo {
     pub zoom: f32,
 }
 
-fn read_layer_info<R: Read>(input: &mut R) -> Result<LayerInfo, ArtError> {
-    Ok(LayerInfo {
-        visible: read_u32(input)? != 0,
-        opacity: read_f32(input)?,
-        name: read_const_string(input)?,
-        action_count: read_u32(input)?,
-        matrix: read_matrix(input)?,
-        zoom: read_f32(input)?,
-    })
-}
-
 pub struct Image {
     pub kind: u32,
     pub raw: Vec<u8>,
 }
 
-fn read_image<R: Read>(input: &mut R) -> Result<Image, ArtError> {
-    let kind = read_u32(input)?;
-    let size = read_u32(input)?;
-    let mut raw = vec![0_u8; size as usize];
-    input.read_exact(&mut raw)?;
-    Ok(Image { kind, raw })
-}
-
 #[derive(Debug)]
 pub enum Action {
-    Stroke { layer: usize, points: Vec<StrokePoint> },
+    Stroke { points: Vec<StrokePoint> },
     _Unknown08(u32),
     PenTransform { matrix: [f32; 16], zoom: f32 },
     PenProperties(PenUpdate),
     PenColor(RGB),
     PenIsEraser(bool),
     PasteLayer(PasteProps),
-    LayerTransform { layer: usize, matrix: [f32; 16], zoom: f32 },
-    CutRect { layer: usize, rect: [f32; 4] },
+    LayerTransform { matrix: [f32; 16], zoom: f32 },
+    CutRect { rect: [f32; 4] },
     LayerMerge {
-        layer: usize, other: usize,
+        other: usize,
         opacity_src: f32, opacity_dst: f32,
         matrix: [f32; 16], zoom: f32,
     },
     DrawImage {
-        layer: usize,
         dst_center: [f32; 2],
         dst_size: [f32; 2],
         _unknown: u32,
@@ -314,95 +108,6 @@ pub enum Action {
         image_id: usize,
     },
     _Unknown05([u8;0x14]),
-}
-
-fn read_action<R: Read>(input: &mut R) -> Result<Action, ArtError> {
-    let layer = read_u32(input)? as usize;
-    let action_tag = read_u32(input)?;
-    let rv = match action_tag {
-        0x01 => {
-            let mut points = Vec::new();
-            let count = read_u32(input)?;
-            if count > 0 {
-                let mut point = StrokePoint {
-                    x: read_f32(input)?,
-                    y: read_f32(input)?,
-                    p: read_f32(input)?,
-                };
-                points.push(point.clone());
-                for _ in 0..count-1 {
-                    let tmp = read_u32(input)?;
-                    let byte = read_u8(input)?;
-                    let dx = (tmp & 0x3fff) as f32 *
-                        if tmp & (1<<14) == 0 { 1.0 } else { -1.0 };
-                    let dy = ((tmp >> 15) & 0x3fff) as f32 *
-                        if tmp & (1<<20) == 0 { 1.0 } else { -1.0 };
-                    let p = ((tmp >> 30) | ((byte as u32) << 2)) as f32;
-                    point.x += dx / 32.0;
-                    point.y += dy / 32.0;
-                    point.p = p / 1023.0;
-                    points.push(point.clone());
-                }
-            }
-            Action::Stroke { layer, points }
-        },
-        0x08 => Action::_Unknown08(read_u32(input)?),
-        0x33 => Action::PenTransform {
-            matrix: read_matrix(input)?,
-            zoom: read_f32(input)?
-        },
-        0x34 => Action::PenProperties(PenUpdate{
-            kind: read_u32(input)?,
-            noise: read_f32(input)?,
-            size: read_f32(input)?,
-            size_min: read_f32(input)?,
-            opacity: read_f32(input)?,
-            opacity_min: read_f32(input)?,
-        }),
-        0x35 => Action::PenColor(read_rgb(input)?),
-        0x36 => Action::PenIsEraser(read_u32(input)? != 0),
-        0x0f => Action::PasteLayer(PasteProps {
-            to_layer: layer,
-            from_layer: read_u32(input)? as usize,
-            rect: read_rect(input)?,
-            matrix1: read_matrix(input)?,
-            zoom1: read_f32(input)?,
-            matrix2: read_matrix(input)?,
-            zoom2: read_f32(input)?,
-        }),
-        0x0d => Action::LayerTransform {
-            layer: layer,
-            matrix: read_matrix(input)?,
-            zoom: read_f32(input)?,
-        },
-        0x0e => Action::CutRect {
-            layer: layer,
-            rect: read_rect(input)?,
-        },
-        0x0c => Action::LayerMerge {
-            layer: layer,
-            other: read_u32(input)? as usize,
-            opacity_src: read_f32(input)?,
-            opacity_dst: read_f32(input)?,
-            matrix: read_matrix(input)?,
-            zoom: read_f32(input)?,
-        },
-        0x07 => Action::DrawImage {
-            layer: layer,
-            dst_center: [read_f32(input)?, read_f32(input)?],
-            dst_size: [read_f32(input)?, read_f32(input)?],
-            _unknown: read_u32(input)?,
-            src_size: [read_u32(input)?, read_u32(input)?],
-            image_id: read_u32(input)? as usize,
-        },
-        0x05 => {
-            let mut buf = [0_u8; 0x14];
-            input.read_exact(&mut buf[..])?;
-            Action::_Unknown05(buf)
-        },
-        _ => return Err(ArtError::BadAction(action_tag)),
-    };
-    Ok(rv)
 }
 
 #[derive(Clone, Debug)]
@@ -424,11 +129,34 @@ pub struct PenUpdate {
 
 #[derive(Debug)]
 pub struct PasteProps {
-    pub to_layer: usize,
     pub from_layer: usize,
     pub rect: [f32; 4],
     pub matrix1: [f32; 16],
     pub zoom1: f32,
     pub matrix2: [f32; 16],
     pub zoom2: f32,
+}
+
+#[derive(Debug)]
+pub enum ArtError {
+    ParsingError(String),
+    IoError(io::Error),
+    //FromUtf8Error(FromUtf8Error),
+    //Utf8Error(Utf8Error),
+    DecompressError(DecodeError),
+    //BadMagic(u32),
+    //BadVersion(u32),
+    //BadAction(u32),
+}
+impl From<io::Error> for ArtError {
+    fn from(err: io::Error) -> ArtError { ArtError::IoError(err) }
+}
+//impl From<FromUtf8Error> for ArtError {
+//    fn from(err: FromUtf8Error) -> ArtError { ArtError::FromUtf8Error(err) }
+//}
+//impl From<Utf8Error> for ArtError {
+//    fn from(err: Utf8Error) -> ArtError { ArtError::Utf8Error(err) }
+//}
+impl From<DecodeError> for ArtError {
+    fn from(err: DecodeError) -> ArtError { ArtError::DecompressError(err) }
 }
---
+++ artparser-rs/src/parser.rs
@@ -0,0 +1,410 @@
+use nom::{
+    IResult,
+    ToUsize,
+    branch::{alt},
+    bytes::complete::{tag, take},
+    combinator::{map, rest_len},
+    error::{ErrorKind, ParseError, VerboseError},
+    number::complete::{le_u8, le_u32, le_f32},
+    multi::{count, length_data, length_value},
+    sequence::{preceded, tuple},
+};
+
+use art::*;
+
+fn length_count<I, O, E, N, C, F>(
+    c: C,
+    f: F,
+) -> impl Fn(I) -> IResult<I, Vec<O>, E>
+where
+    I: Clone + PartialEq,
+    N: ToUsize,
+    C: Fn(I) -> IResult<I, N, E>,
+    F: Fn(I) -> IResult<I, O, E>,
+    E: ParseError<I>,
+{
+    move |i: I| {
+        let mut input = i.clone();
+
+        let count = match c(input.clone()) {
+            Ok((i, o)) => {
+                input = i;
+                o.to_usize()
+            }
+            Err(nom::Err::Error(e)) => {
+                return Err(nom::Err::Error(E::append(i, ErrorKind::Count, e)));
+            }
+            Err(e) => {
+                return Err(e);
+            }
+        };
+
+        let mut res = Vec::new();
+
+        for _ in 0..count {
+            let input_ = input.clone();
+            match f(input_) {
+                Ok((i, o)) => {
+                    res.push(o);
+                    input = i;
+                }
+                Err(nom::Err::Error(e)) => {
+                    return Err(nom::Err::Error(E::append(i, ErrorKind::Count, e)));
+                }
+                Err(e) => {
+                    return Err(e);
+                }
+            }
+        }
+
+        Ok((input, res))
+    }
+}
+
+pub fn read_compressed<'a>(i: &'a[u8])
+-> IResult<&'a[u8], (usize, Vec<ArtPin>, usize), VerboseError<&'a[u8]>>
+{
+    let ver00 = map(tuple((tag(b"\x00\x00\x00\x00"), take(0x08_u8))),
+                    |_| (0x00_usize, Vec::new()));
+    let ver81 = map(tuple((tag(b"\x81\x00\x00\x00"), take(0x1c_u8))),
+                    |_| (0x81_usize, Vec::new()));
+    let ver82 = map(tuple((tag(b"\x82\x00\x00\x00"), take(0x21_u8),
+                          length_count(le_u32, read_pin))),
+                    |t| (0x82_usize, t.2));
+
+    let file = tuple((
+        alt((tag(b"\xc5\xb3\x8b\xe7"), tag(b"\xc5\xb3\x8b\xe9"))),
+        alt((ver00, ver81, ver82)),
+        le_u32,
+    ));
+
+    map(file, |(_, (ver, pins), len)| {
+        (ver, pins, len.to_usize())
+    })(i)
+}
+
+pub fn read_content<'a>(i: &'a[u8])
+-> IResult<&'a[u8], ArtFile, VerboseError<&'a[u8]>>
+{
+    let file = tuple((
+        le_u32,
+        le_u32,
+        le_u32,
+        read_rgb,
+        le_f32,
+        le_u32, le_u32, le_u32, le_u32,
+        read_pen,
+        le_u32, le_u32,
+        read_matrix,
+        le_f32,
+        length_count(le_u32, map(le_u32, |t|t.to_usize())),
+        length_count(le_u32, read_layer_info),
+        length_count(le_u32, read_image),
+        length_count(le_u32, read_action),
+        rest_len,
+    ));
+
+    map(file, |t| {
+        ArtFile {
+            version: t.0.to_usize(),
+            active_layer: t.1.to_usize(),
+            background_color: t.3,
+            background_alpha: t.4,
+            pen_info: t.9,
+            view_matrix: t.12,
+            view_zoom: t.13,
+            pins: Vec::new(),
+            layer_order: t.14,
+            layers: t.15,
+            images: t.16,
+            actions: t.17,
+        }
+    })(i)
+}
+
+fn read_matrix<'a>(i: &'a[u8])
+-> IResult<&'a[u8], [f32; 16], VerboseError<&'a[u8]>>
+{
+    map(count(le_f32, 16), |v| {
+        let mut m = [0.0_f32; 16];
+        m.copy_from_slice(&v[..]);
+        m
+    })(i)
+}
+
+fn read_rect<'a>(i: &'a[u8])
+-> IResult<&'a[u8], [f32; 4], VerboseError<&'a[u8]>>
+{
+    map(tuple((le_f32, le_f32, le_f32, le_f32)),
+        |t| [t.0, t.1, t.2, t.3]
+    )(i)
+}
+
+fn read_rgb<'a>(i: &'a[u8])
+-> IResult<&'a[u8], RGB, VerboseError<&'a[u8]>>
+{
+    map(tuple((le_u8, le_u8, le_u8)), |t| {
+        RGB {
+            r: t.0,
+            g: t.1,
+            b: t.2,
+        }
+    })(i)
+}
+
+fn read_pen<'a>(i: &'a[u8])
+-> IResult<&'a[u8], PenInfo, VerboseError<&'a[u8]>>
+{
+    map(tuple((le_u32, read_rgb, le_f32, le_f32,
+               le_f32, le_f32,   le_f32, le_u32)),
+        |t| PenInfo {
+            kind: t.0,
+            color: t.1,
+            noise: t.2,
+            size: t.3,
+            size_min: t.4,
+            opacity: t.5,
+            opacity_min: t.6,
+            is_eraser: t.7 != 0,
+        }
+    )(i)
+}
+
+fn read_pin<'a>(i: &'a[u8])
+-> IResult<&'a[u8], ArtPin, VerboseError<&'a[u8]>>
+{
+    map(tuple((read_matrix, read_str)), |(matrix, name)| {
+        ArtPin { matrix, name: name.trim().to_string() }
+    })(i)
+}
+
+fn read_str<'a>(i: &'a[u8])
+-> IResult<&'a[u8], &'a str, VerboseError<&'a[u8]>>
+{
+    length_value(le_u32, |i| Ok((b"", std::str::from_utf8(i).expect("couldn't read str?"))))(i)
+}
+
+fn read_const_str<'a>(i: &'a[u8])
+-> IResult<&'a[u8], &'a str, VerboseError<&'a[u8]>>
+{
+    map(take(256_usize), |buf: &[u8]| {
+        let start = buf.split(|&c| c == 0).next().unwrap();
+        std::str::from_utf8(&start).expect("couldn't read const str?")
+    })(i)
+}
+
+fn read_layer_info<'a>(i: &'a[u8])
+-> IResult<&'a[u8], LayerInfo, VerboseError<&'a[u8]>>
+{
+    map(tuple((le_u32, le_f32, read_const_str, le_u32, read_matrix, le_f32)), |t| {
+        LayerInfo {
+            visible: t.0 != 0,
+            opacity: t.1,
+            name: t.2.to_string(),
+            action_count: t.3,
+            matrix: t.4,
+            zoom: t.5,
+        }
+    })(i)
+}
+
+fn read_image<'a>(i: &'a[u8])
+-> IResult<&'a[u8], Image, VerboseError<&'a[u8]>>
+{
+    map(tuple((le_u32, length_data(le_u32))), |(kind, slice)| {
+        Image { kind, raw: slice.to_vec() }
+    })(i)
+}
+
+fn read_action<'a>(i: &'a[u8])
+-> IResult<&'a[u8], (usize, Action), VerboseError<&'a[u8]>>
+{
+    tuple((
+        map(le_u32, |i|i.to_usize()),
+        alt((
+            read_action_stroke,
+            read_action_05,
+            read_action_08,
+            read_action_pen_transform,
+            read_action_pen_props,
+            read_action_pen_color,
+            read_action_pen_is_eraser,
+            read_action_paste_layer,
+            read_action_layer_transform,
+            read_action_cut_rect,
+            read_action_layer_merge,
+            read_action_draw_image,
+        ))
+    ))(i)
+}
+
+fn read_action_stroke<'a>(i: &'a[u8])
+-> IResult<&'a[u8], Action, VerboseError<&'a[u8]>>
+{
+    let (mut input, count) = preceded(tag(b"\x01\x00\x00\x00"), le_u32)(i)?;
+    let mut points = Vec::new();
+    if count > 0 {
+        let (i, mut point) = map(tuple((le_f32, le_f32, le_f32)), |t| {
+            StrokePoint { x: t.0, y: t.1, p: t.2 }
+        })(input)?;
+        input = i;
+
+        points.push(point.clone());
+        for _ in 0..count-1 {
+            let (i, (tmp32, tmp8)) = tuple((le_u32, le_u8))(input)?;
+            input = i;
+
+            let dx = (tmp32 & 0x3fff) as f32 *
+                if tmp32 & (1<<14) == 0 { 1.0 } else { -1.0 };
+            let dy = ((tmp32 >> 15) & 0x3fff) as f32 *
+                if tmp32 & (1<<20) == 0 { 1.0 } else { -1.0 };
+            let p = ((tmp32 >> 30) | ((tmp8 as u32) << 2)) as f32;
+            point.x += dx / 32.0;
+            point.y += dy / 32.0;
+            point.p = p / 1023.0;
+            points.push(point.clone());
+        }
+    }
+    Ok((input, Action::Stroke { points }))
+}
+
+fn read_action_pen_transform<'a>(i: &'a[u8])
+-> IResult<&'a[u8], Action, VerboseError<&'a[u8]>>
+{
+    preceded(
+        tag(b"\x33\x00\x00\x00"),
+        map(tuple((read_matrix, le_f32)),
+            |(matrix, zoom)| Action::PenTransform { matrix, zoom })
+    )(i)
+}
+
+fn read_action_pen_props<'a>(i: &'a[u8])
+-> IResult<&'a[u8], Action, VerboseError<&'a[u8]>>
+{
+    preceded(
+        tag(b"\x34\x00\x00\x00"),
+        map(tuple((le_u32, le_f32, le_f32, le_f32, le_f32, le_f32)),
+            |t| Action::PenProperties(PenUpdate {
+                kind: t.0,
+                noise: t.1,
+                size: t.2,
+                size_min: t.3,
+                opacity: t.4,
+                opacity_min: t.5,
+            }))
+    )(i)
+}
+
+fn read_action_pen_color<'a>(i: &'a[u8])
+-> IResult<&'a[u8], Action, VerboseError<&'a[u8]>>
+{
+    preceded(
+        tag(b"\x35\x00\x00\x00"),
+        map(read_rgb, Action::PenColor)
+    )(i)
+}
+
+fn read_action_pen_is_eraser<'a>(i: &'a[u8])
+-> IResult<&'a[u8], Action, VerboseError<&'a[u8]>>
+{
+    preceded(
+        tag(b"\x36\x00\x00\x00"),
+        map(le_u32, |v| Action::PenIsEraser(v != 0))
+    )(i)
+}
+
+fn read_action_paste_layer<'a>(i: &'a[u8])
+-> IResult<&'a[u8], Action, VerboseError<&'a[u8]>>
+{
+    preceded(
+        tag(b"\x0f\x00\x00\x00"),
+        map(tuple((le_u32, read_rect,
+                   read_matrix, le_f32,
+                   read_matrix, le_f32)),
+            |t| Action::PasteLayer(PasteProps {
+                from_layer: t.0.to_usize(),
+                rect: t.1,
+                matrix1: t.2,
+                zoom1: t.3,
+                matrix2: t.4,
+                zoom2: t.5,
+            }))
+    )(i)
+}
+
+fn read_action_layer_transform<'a>(i: &'a[u8])
+-> IResult<&'a[u8], Action, VerboseError<&'a[u8]>>
+{
+    preceded(
+        tag(b"\x0d\x00\x00\x00"),
+        map(tuple((read_matrix, le_f32)),
+            |(matrix, zoom)| Action::LayerTransform {
+                matrix, zoom,
+            })
+    )(i)
+}
+
+fn read_action_cut_rect<'a>(i: &'a[u8])
+-> IResult<&'a[u8], Action, VerboseError<&'a[u8]>>
+{
+    preceded(
+        tag(b"\x0e\x00\x00\x00"),
+        map(read_rect, |rect| Action::CutRect { rect })
+    )(i)
+}
+
+fn read_action_layer_merge<'a>(i: &'a[u8])
+-> IResult<&'a[u8], Action, VerboseError<&'a[u8]>>
+{
+    preceded(
+        tag(b"\x0c\x00\x00\x00"),
+        map(tuple((le_u32, le_f32, le_f32,
+                    read_matrix, le_f32)),
+            |t| Action::LayerMerge {
+                other: t.0.to_usize(),
+                opacity_src: t.1,
+                opacity_dst: t.2,
+                matrix: t.3,
+                zoom: t.4,
+            })
+    )(i)
+}
+
+fn read_action_draw_image<'a>(i: &'a[u8])
+-> IResult<&'a[u8], Action, VerboseError<&'a[u8]>>
+{
+    preceded(
+        tag(b"\x07\x00\x00\x00"),
+        map(tuple((le_f32, le_f32, le_f32, le_f32,
+                   le_u32, le_u32, le_u32, le_u32)),
+            |t| Action::DrawImage {
+                dst_center: [t.0, t.1],
+                dst_size: [t.2, t.3],
+                _unknown: t.4,
+                src_size: [t.5, t.6],
+                image_id: t.7.to_usize(),
+            })
+    )(i)
+}
+
+fn read_action_05<'a>(i: &'a[u8])
+-> IResult<&'a[u8], Action, VerboseError<&'a[u8]>>
+{
+    preceded(
+        tag(b"\x05\x00\x00\x00"),
+        map(take(0x14_usize), |t| {
+            let mut buf = [0_u8; 0x14];
+            buf.copy_from_slice(t);
+            Action::_Unknown05(buf)
+        })
+    )(i)
+}
+
+fn read_action_08<'a>(i: &'a[u8])
+-> IResult<&'a[u8], Action, VerboseError<&'a[u8]>>
+{
+    preceded(
+        tag(b"\x08\x00\x00\x00"),
+        map(le_u32, Action::_Unknown08)
+    )(i)
+}
